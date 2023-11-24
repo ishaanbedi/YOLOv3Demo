7499cf75827363b38a6d251f7dc760da6f21a31e
Ishaan Bedi <bediishaan6@gmail.com>
2023-11-24 11:35:40 +0530

    Initial Commit

---
+++ YOLOv3Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YOLOv3Demo
+//
+//  Created by Ishaan Bedi on 24/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YOLOv3Demo/YOLOv3DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOLOv3DemoApp.swift
+//  YOLOv3Demo
+//
+//  Created by Ishaan Bedi on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct YOLOv3DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
